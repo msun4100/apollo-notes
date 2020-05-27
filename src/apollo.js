aec99686875e124d728dd7670e3e2f1acc1f7f3b
msun4100 <msun4100@gmail.com>
2020-05-27 16:11:41 +0900

    Offline Apollo Configurations

---
+++ src/apollo.js
@@ -0,0 +1,23 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { withClientState } from "apollo-link-state";
+import { ApolloLink } from "apollo-link";
+
+import { typeDefs, defaults, resolvers } from "./clientState";
+
+const cache = new InMemoryCache();
+
+const stateLink = withClientState({
+  cache,
+  typeDefs,
+  defaults,
+  resolvers,
+});
+
+// apollo-boost 는 http링크가 필요하기 때문에 stateLink 커스터마이징
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([stateLink]),
+});
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo";
+import GlobalStyles from "./globalStyles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
